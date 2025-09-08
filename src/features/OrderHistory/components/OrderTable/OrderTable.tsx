64a43fc6f71425b196af62a2fc6bb340ce2a1af3
Meredith Frazier Britt <118227496+mfrazb@users.noreply.github.com>
2025-09-08 16:18:39 -0500

    refactor: signin to separate component

--- src/features/OrderHistory/components/OrderTable/OrderTable.tsx
+++ src/features/OrderHistory/components/OrderTable/OrderTable.tsx
@@ -1,17 +1,10 @@
 import { Card } from '@radix-ui/themes'
-<<<<<<< HEAD
 // import type { Order } from '../../../../data'
 
 // export const OrderTable = ({ orders }: { orders: Order[] }) => {
 //   return <Card>Order Table</Card>
 // }
-=======
->>>>>>> c7da168 (feat: set up order history section)
 
 export const OrderTable = () => {
   return <Card>Order Table</Card>
 }
-<<<<<<< HEAD
-
-=======
->>>>>>> c7da168 (feat: set up order history section)
